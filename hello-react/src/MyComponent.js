7fe53f3310b1a98b851e1020f0f003331f23daa4
soy22 <jsy03070@naver.com>
2022-10-27 13:29:56 +0900

    3.3.7-2

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types';
 
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름'
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+  };
   render() {
     const { name, favoriteNumber, children } = this.props; // 비구조화 할당
     return (
@@ -16,19 +23,6 @@ class MyComponent extends Component {
     );
   }
 }
-
-
-
-MyComponent.defaultProps = {
-  name: '기본 이름'
-};
-
-
-
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired
-};
 
 
 
