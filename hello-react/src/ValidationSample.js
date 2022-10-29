e08b70ccfdb951e250b5f7bbebcd19d268f71123
soy22 <jsy03070@naver.com>
2022-10-29 17:49:31 +0900

    5.2.3.2

--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -19,13 +19,13 @@ handleChange = (e) => {
 
 
 
-handleButtonClick = () => {
+  handleButtonClick = () => {
     this.setState({
       clicked: true,
       validated: this.state.password === '0000'
-    })
+    });
+    this.input.focus();
   }
-
 
 
 render() {
