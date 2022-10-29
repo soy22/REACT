ff467e15b29b155ea31658b358e73701471cbab2
soy22 <jsy03070@naver.com>
2022-10-29 12:16:43 +0900

    4.2.3.1

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -11,6 +11,31 @@ state = {
 
 
 
+constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+
+handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+
+
+handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
+
+
+
 render() {
     return (
       <div>
@@ -20,22 +45,9 @@ render() {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={
-            (e) => {
-              this.setState({
-                message: e.target.value
-              })
-            }
-          }
+          onChange={this.handleChange}
         />
-        <button onClick={
-          () => {
-            alert(this.state.message);
-            this.setState({
-              message: ''
-            });
-          }
-        }>확인</button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
