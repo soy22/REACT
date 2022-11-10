e7eae5ebda80162fab5b598cb5817d76cd04bc00
soy22 <jsy03070@naver.com>
2022-11-10 20:54:31 +0900

    8.2

--- hello-react/src/Info.js
+++ hello-react/src/Info.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname
+    });
+  });
  
   const onChangeName = e => {
     setName(e.target.value);
   };
- 
-  const onChangeNickname = e => {
+
+
+
+const onChangeNickname = e => {
     setNickname(e.target.value);
   };
- 
+
+
+
   return (
     <div>
       <div>
@@ -30,7 +41,6 @@ const Info = () => {
     </div>
   );
 };
-
-
+ 
 
 export default Info;
