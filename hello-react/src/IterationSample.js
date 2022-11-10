e5415f46bde8cd7dd69cb3710588269bd16e941c
soy22 <jsy03070@naver.com>
2022-11-10 19:44:07 +0900

    6.4.3

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -21,10 +21,15 @@ const IterationSample = () => {
     setNames(nextNames); // names 값을 업데이트한다.
     setInputText(''); // inputText를 비운다.
   };
-
-
-
-const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+  const onRemove = id => {
+    const nextNames = names.filter(name => name.id !== id);
+    setNames(nextNames);
+  };
+  const namesList = names.map(name => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
   return (
     <>
       <input value={inputText} onChange={onChange} />
