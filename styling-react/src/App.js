9f9c4aedfbbee5d4ab5ceffbd5e25920b0ebd885
soy22 <jsy03070@naver.com>
2022-11-11 10:23:56 +0900

    9.1.2-2

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
+ 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
+          <img src={logo} className="logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -25,7 +23,5 @@ class App extends Component {
     );
   }
 }
-
-
-
+ 
 export default App;
