aceb376e0659acfb6e1702ed1fc96c09eddfc1aa
soy22 <jsy03070@naver.com>
2022-11-11 13:03:22 +0900

    10.3.3.3

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
- 
-const TodoList = ({ todos }) => {
+
+
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map(todo => (
-        <TodoListItem todo={todo} key={todo.id} />
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
       ))}
     </div>
   );
 };
- 
+
+
+
 export default TodoList;
