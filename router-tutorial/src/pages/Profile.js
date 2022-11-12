902543b52a33b80d176b2f2b20b2c21a49b4b486
soy22 <jsy03070@naver.com>
2022-11-12 10:49:05 +0900

    13.6.4

--- router-tutorial/src/pages/Profile.js
+++ router-tutorial/src/pages/Profile.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
-import WithRouterSample from './WithRouterSample';
- 
+
+
 const Profiles = () => {
-  return (
-    <div>
-      <h3>사용자 목록:</h3>
-      <ul>
-        <li>
-          <Link to="/profiles/velopert">velopert</Link>
-        </li>
-        <li>
-          <Link to="/profiles/gildong">gildong</Link>
-        </li>
-      </ul>
- 
-      <Route
-        path="/profiles"
-        exact
-        render={() => <div>사용자를 선택해 주세요.</div>}
-      />
-      <Route path="/profiles/:username" component={Profile} />
-      <WithRouterSample />
-    </div>
-  );
+  const activeStyle = {
+    background: 'black',
+    color: 'white'
 };
- 
+return (
+  <div>
+    <h3>사용자 목록:</h3>
+    <ul>
+      <li>
+        <NavLink activeStyle={activeStyle} to="/profiles/velopert" active>
+          velopert
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+          gildong
+        </NavLink>
+      </li>
+    </ul>
+  </div>
+);
+};
+
 export default Profiles;
