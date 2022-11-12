47ea9a45e0fc0f92658ba6348fb632bcb53e241f
soy22 <jsy03070@naver.com>
2022-11-12 10:13:54 +0900

    13.2.4

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
  
 const App = () => {
   return (
-    <div>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </div>
+    <Routes>
+      <Route path="/" component={<Home />} />
+      <Route path="/about" component={<About />} />
+    </Routes>
   );
 };
 
