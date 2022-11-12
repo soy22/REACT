efe103ce7e6ee00c3752ad25ed900aa1b4d675e1
soy22 <jsy03070@naver.com>
2022-11-12 10:20:13 +0900

    13.3-1

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
@@ -7,17 +7,9 @@ import Home from './Home';
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-      </ul>
-      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
+      <Route path="/info" component={About} />
     </div>
   );
 };
