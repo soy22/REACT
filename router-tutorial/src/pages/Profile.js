af0a14b8b243b0e240e4d75da19d477d76154144
soy22 <jsy03070@naver.com>
2022-11-12 10:36:17 +0900

    13.5-1

--- router-tutorial/src/pages/Profile.js
+++ router-tutorial/src/pages/Profile.js
@@ -1,33 +1,28 @@
-import { useParams } from 'react-router-dom';
-
-const data = {
-    velopert: {
-      name: '김민준',
-      description: '리액트를 좋아하는 개발자'
-    },
-    gildong: {
-      name: '홍길동',
-      description: '고전 소설 홍길동전의 주인공'
-    }
-  };
-  
-  const Profile = () => {
-    const params = useParams();
-    const profile = data[params.username];
-
-    return (
-      <div>
-        <h1>사용자 프로필</h1>
-        {profile ? (
-            <div>
-                <h2>{profile.name}</h2>
-                <p>{profile.description}</p>
-            </div>
-        ) : (
-          <p>존재하지 않는 프로필입니다.</p>
-        )}
-        </div>
-        );
-     };
-  
-  export default Profile;
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import Profile from './Profile';
+ 
+const Profiles = () => {
+  return (
+    <div>
+      <h3>사용자 목록:</h3>
+      <ul>
+        <li>
+          <Link to="/profiles/velopert">velopert</Link>
+        </li>
+        <li>
+          <Link to="/profiles/gildong">gildong</Link>
+        </li>
+      </ul>
+ 
+      <Route
+        path="/profiles"
+        exact
+        render={() => <div>사용자를 선택해 주세요.</div>}
+      />
+      <Route path="/profiles/:username" component={Profile} />
+    </div>
+  );
+};
+ 
+export default Profiles;
