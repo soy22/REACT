679f0e1295e7476b42b65c47a0751ed5a4635861
soy22 <jsy03070@naver.com>
2022-11-12 10:20:27 +0900

    13.3-2

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-
-
+ 
 const App = () => {
   return (
     <div>
       <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
-      <Route path="/info" component={About} />
+      <Route path={['/about', '/info']} component={About} />
     </div>
   );
 };
-
-
-
+ 
 export default App;
