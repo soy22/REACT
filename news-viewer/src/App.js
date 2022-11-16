55a6732cee8441df5e884f58a48b2f3afc84336f
soy22 <jsy03070@naver.com>
2022-11-16 19:35:29 +0900

    14.3

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
- 
+
+
 const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=0a8c4202385d4ec1bb93b7e277b3c51f',
       );
       setData(response.data);
     } catch (e) {
@@ -18,9 +19,11 @@ const App = () => {
       <div>
         <button onClick={onClick}>불러오기</button>
       </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
     </div>
   );
 };
- 
+
+
+
 export default App;
