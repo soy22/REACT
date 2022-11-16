6c41f5a744b637fed6a1fbd8830bec675aa9001b
soy22 <jsy03070@naver.com>
2022-11-16 19:34:09 +0900

    14.2-2

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
-
+ 
 const App = () => {
   const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
       setData(response.data);
-    });
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
@@ -18,7 +22,5 @@ const App = () => {
     </div>
   );
 };
-
-
-
+ 
 export default App;
