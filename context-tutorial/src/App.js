b2771b7a67f6c62bd2536f607747a2166dd7e329
soy22 <jsy03070@naver.com>
2022-11-19 23:46:10 +0900

    15.2.3-2

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
 import ColorContext from './contexts/color';
+ 
 const App = () => {
   return (
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorContext.Provider>
       <div>
         <ColorBox />
       </div>
     </ColorContext.Provider>
   );
 };
-
-
+ 
 export default App;
